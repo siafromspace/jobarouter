5cf4870428df47a4ca4a467cf954022cddd1d2e2
siafromspace <sharonyak15@gmail.com>
2023-02-25 16:42:31 +0100

    old react router

---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+const About = () => {
+    return ( 
+        <div className="about">
+            <h2>About Us</h2>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni ab enim nam nemo animi consectetur neque eius doloribus, atque ipsum expedita, quod consequatur possimus porro, tenetur ipsa itaque distinctio laborum accusamus incidunt at optio dicta architecto sunt! Corrupti sit odio reiciendis quis, quia, reprehenderit soluta dolorem delectus, ea dignissimos nobis!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi deleniti laborum nulla modi necessitatibus blanditiis, vitae quae consequatur omnis itaque, doloremque cumque! Cumque, hic blanditiis cum dolores voluptates nisi exercitationem, suscipit eius commodi molestias dolor corrupti tenetur labore, repudiandae iusto eligendi soluta quod consequuntur reprehenderit itaque repellat expedita inventore! Sit.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus sint veniam, accusantium ipsa consequuntur veritatis, excepturi architecto tempore ratione tempora unde totam recusandae fuga, dolor in eum iste iure eveniet blanditiis. Laudantium voluptate cum, illum earum praesentium nihil modi reprehenderit et vitae inventore aspernatur nemo quod officiis quidem iusto nesciunt.</p>
+        </div>
+     );
+}
+ 
+export default About;
